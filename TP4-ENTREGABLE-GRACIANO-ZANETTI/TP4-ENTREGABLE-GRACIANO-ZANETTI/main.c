a8395ecfe31794dbf286924009d0a4c0c083c911
BrunoZanetti09 <144629735+BrunoZanetti09@users.noreply.github.com>
2024-07-03 20:00:14 -0300

    Add files via upload

--- TP4-ENTREGABLE-GRACIANO-ZANETTI/TP4-ENTREGABLE-GRACIANO-ZANETTI/main.c
+++ TP4-ENTREGABLE-GRACIANO-ZANETTI/TP4-ENTREGABLE-GRACIANO-ZANETTI/main.c
@@ -7,45 +7,81 @@
 #include <util/delay.h>
 #include "serialPort.h"
 #include "PWMsoft.h"
-
 #define BR9600 0x67	// 0x67=103 configura BAUDRATE=9600@16MHz
 
+//funciones ADC
+void adc_init();
+uint16_t adc_read();
+
+//funcion UART
+void configurarUART();
+
+//Funcion PWM
 void set_pwm(uint8_t red, uint8_t green, uint8_t blue);
-volatile uint8_t red = 255;
-volatile uint8_t selected_color;
+
+//Variables globales
+volatile uint8_t red = 0;
+volatile uint8_t selected_color = 'R';
+uint8_t pwm_value;
+
 //TIMERS
 void configurarTimer1();
 void configurarTimer0();
 
 int main(void)
 {  	DDRB |= (1<<PORTB5) | (1<<PORTB1)| (1<<PORTB2);
+	DDRC &= ~(1<<PORTC3);
+	adc_init();
 	configurarTimer0();
 	configurarTimer1();
-    configurarUART();
+    SerialPort_Init(BR9600);   // Inicializo formato 8N1 y BAUDRATE = 9600bps
+    SerialPort_TX_Enable();		// Activo el transmisor del Puerto Serie
+    SerialPort_RX_Enable();			// Activo el Receptor del Puerto Serie
+    SerialPort_RX_Interrupt_Enable(); // habilitacion de las interrupciones del receptor
 	sei();								// habilitacion de interrupciones globales
-	uint8_t green = 255, blue = 255;
-	
+	uint8_t green = 0, blue = 0;
+
     while (1) 
     {
-
-
-	 /*  switch (selected_color) {
+		
+		if (selected_color != 'E') {
+	   
+	    pwm_value = adc_read();
+		
+         switch (selected_color) {
 	       case 'R':
 	       red = pwm_value;
 	       break;
 	       case 'G':
-	       green = pwm_value;
+	      green = pwm_value;
 	       break;
 	       case 'B':
 	       blue = pwm_value;
 	       break;
        }
-*/	   // Establecer el valor PWM
+// Establecer el valor PWM
 	   set_pwm(red, green, blue);
     }
+	}
 return 0;
 }
 
+uint16_t adc_read() {
+	ADCSRA |= (1 << ADSC); // Iniciar la conversión
+	
+	while((ADCSRA&(1<<ADIF))==0);//wait for conversion to finish
+	
+	ADCSRA |= (1<<ADIF); //borrar flag
+	
+	return ADCH;
+}
+	
+void adc_init() {
+	// Configurar el ADC
+	ADMUX |= (1 << REFS0) | (1 << MUX3) | (1<< ADLAR); // AVcc con capacitor en AREF y ADC3
+	ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1)| (1 << ADPS0); // Habilitar ADC y prescaler de 128
+	DIDR0 |= (1 << ADC3D); // Deshabilitar la entrada digital en ADC3
+}
 
 // Función para configurar el Timer1
 void configurarTimer1() {
